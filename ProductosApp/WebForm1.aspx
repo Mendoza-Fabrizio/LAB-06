944c7bab324568964bb68f0e4ad83f70352a8501
Alexander <aacu406@gmail.com>
2021-10-06 22:11:12 -0500

    EjercicioPropuesto

---
+++ ProductosApp/WebForm1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ProductosApp.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="card cardContainer mt-4">
+        <div class="card-header">
+            <h1>LISTA DE PRODUCTOS </h1>
+        </div>
+        <div class="p-3">
+            <!-- FORMULARIO PARA BUSCAR -->
+            <div>
+                <asp:Label class="form-label" ID="Label2" runat="server" Text="Codigo"></asp:Label>
+                <asp:TextBox class="form-control" ID="txtCodigo" runat="server"></asp:TextBox>
+
+                <div class="row">
+
+                    <div class="col-4 d-grid gap-2">
+                        <asp:Button class="btn btn-success mt-1" ID="btnBuscar" runat="server" Text="Buscar" OnClick="btnBuscar_Click" />                       
+                    </div>
+                    <div class="col-4 d-grid gap-2">
+                         <asp:Button class="btn btn-danger mt-1" ID="btnLimpiar" runat="server" Text="Limpiar" OnClick="btnMostrar_limpiar"/>
+                    </div>
+                    <div class="col-4 d-grid gap-2">
+                        <asp:Button class="btn btn-primary mt-1" ID="btnAdd" runat="server" Text="Agregar" OnClick="btnMostrar_guardar"/>
+                    </div>
+                </div>
+
+            </div>
+            <!-- FORMULARIO PARA AGREGAR -->
+            <div class="row">
+                <div class="col-12">
+                    <asp:Label class="form-label" ID="Label1" runat="server" Text="Nombre"></asp:Label>
+                    <asp:TextBox class="form-control" ID="txtNombre" runat="server"></asp:TextBox>
+                
+
+                    <asp:Label class="form-label" ID="Label3" runat="server" Text="Precio"></asp:Label>
+                    <asp:TextBox class="form-control" ID="txtPrecio" runat="server" type="number" step="0.01" min="0"></asp:TextBox>
+                    
+
+                    <asp:Label class="form-label" ID="Label4" runat="server" Text="Stock"></asp:Label>
+                    <asp:TextBox class="form-control" ID="txtStock" runat="server" type="number" step="0.01" min="0"></asp:TextBox>
+                   
+                </div>
+            </div>
+
+            <div class=" d-grid gap-2">
+                <asp:Button  class="btn btn-dark mt-1 mb-1" ID="btnMostrar" runat="server" Text="Mostrar todos los productos" OnClick="btnMostrar_Click" />
+            </div>
+            <!-- LABEL DE RESULTADOS -->
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+            <!-- TABLA -->
+            <asp:GridView class="table" ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ ProductosApp/WebForm1.aspx.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ProductosApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        /* STRING DE CONEXIÓN */
+        private string connectionString = "Data Source=ALEXANDER\\SQLEXPRESS;" +
+            "Initial Catalog=VENTAS;Integrated Security=SSPI";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /* FUNCION PARA BUSCAR REGISTROS ESPECIFICOS */
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string sqlSelect = "SELECT * FROM PRODUCTOS WHERE codpro = " + txtCodigo.Text;
+            SqlConnection connection = new SqlConnection(this.connectionString);
+            SqlCommand command = new SqlCommand(sqlSelect, connection);
+            SqlDataReader dr;
+            try
+            {
+                connection.Open();
+                dr = command.ExecuteReader();
+                if (dr.Read())
+                {
+                    /* EL RESULTADO DE LA BUSQUEDA SE MUESTRA EN EL LABEL */
+                    lblResult.Text = dr[1].ToString() + " - " + dr[2].ToString() + " - " + dr[3].ToString();
+                }
+                connection.Close();
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                lblResult.Text = "Error a la hora de buscar el producto: ";
+                lblResult.Text += err.Message;
+            }
+        }
+
+        /* FUNCION PARA MOSTRAR TODOS LOS PRODUCTOS DE LA TABLA */
+        protected void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string SqlSelect = "SELECT codpro AS Codigo, nompro AS Nombre, precio AS Precio, Stock FROM PRODUCTOS";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(SqlSelect, connection);
+            SqlDataReader dr;
+            try
+            {
+                connection.Open();
+                dr = command.ExecuteReader();
+                /* SE RELLENA LA DATA EN EL GRIDVIEW */
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                connection.Close();
+
+            }
+            catch (Exception err)
+            {
+                lblResult.Text = "Error al recuperar los productos - ";
+                lblResult.Text += err.Message;
+            }
+        }
+        /* LIMPIA LOS CAMPOS AL APRETAR EL BOTON DE LIMPIAR */
+        protected void btnMostrar_limpiar(object sender, EventArgs e)
+        {
+            this.limpiar();
+        }
+        private void limpiar()
+        {
+            lblResult.Text = "";
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtStock.Text = "";
+        }
+
+        /* VALIDA LOS CAMPOS QUE NO PUEDEN SER VALIDADOS POR LOS CONTROLES DE VALIDACION */
+        private bool valid()
+        {
+
+            if (txtNombre.Text != "") ;
+            {
+                double price;
+                bool isDouble = Double.TryParse(txtPrecio.Text, out price);
+                if (isDouble)
+                {
+                    double stock;
+                    bool isDouble2 = Double.TryParse(txtPrecio.Text, out stock);
+                    if (isDouble2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /* GUARDA LOS REGISTROS EN LA TABLA PRODUCTO */
+        protected void btnMostrar_guardar(object sender, EventArgs e)
+        {
+            if (this.valid())
+            {
+                string SqlSelect = "INSERT INTO PRODUCTOS values('" + txtNombre.Text + "', " + txtPrecio.Text + "," + txtStock.Text + ")";
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(SqlSelect, connection);
+                SqlDataReader dr;
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    lblResult.Text = "Agregado con exito...";
+                    connection.Close();
+                    this.limpiar();
+                }
+                catch (Exception err)
+                {
+                    lblResult.Text = "Error al agregar: ";
+                    lblResult.Text += err.Message;
+                }
+            }
+            else
+            {
+                lblResult.Text = "Parece que algo esta mal.";
+            }
+
+        }
+    }
+}
